4d5e4f059191658909a88b171e7d2a76d36c9dc2
Gaurav Chauhan <130596151+GauravChauhan1998@users.noreply.github.com>
2025-08-12 09:48:27 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";      //treat all JS Code as newer version
+
+//alert(3+3)        // we are using node js not browser
+
+console.log(3
+    +3)              //code readibility should be given importance
+
+console.log("Gaurav");
+
+//tc39.es.     original documentation for javascript
+
+//PRIMITIVE DATA-TYPES
+
+let name = "Gaurav"
+let age = 17
+let isLogged = true
+
+/*
+    1. Number -> 2 - 2 power 53
+    2. bigint
+    3. String -> ""
+    4. boolean -> true/false
+    5. null -> standalone value
+    6. undefined
+    7. symbol -> used for uniqueness
+*/
+
+// OBJECTS
+
+console.log(typeof null);        //object
+console.log(typeof undefined);    //undefined
+
+
+
