7c7fe06a42bf74ddab59f3930c29bd75652a0652
Gaurav Chauhan <130596151+GauravChauhan1998@users.noreply.github.com>
2025-08-12 10:38:01 +0000

    conversionoperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,72 @@
+let score = "33";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+//console.log(typeof(valueInNumber));
+
+// "33" -> 33
+// "33abc" -> NaN (Not a Number)
+// true -> 1;  false -> 0
+
+let isLoggedIn = ""
+
+let booleanIsLogged = Boolean(isLoggedIn)
+
+//console.log(booleanIsLogged);
+
+// 1 -> true
+// 0 -> false
+// "" -> false
+// "Gaurav" -> true
+
+let someNumber = 43
+
+let stringNumber = String(someNumber);
+
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ************************************* OPERATIONS *******************************************
+
+
+let value = 3;
+let negValue = -value
+//console.log(negValue)
+
+//basic math operations
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) //2 to the power 3
+// console.log(2/3) 
+// console.log(2%3) 
+
+
+let str1 = "hello"
+let str2 = " Gaurav"
+let str3 = str1 + str2
+//console.log(str3);          // hello Gaurav
+
+// console.log("1" + 2)         //12
+// console.log(1 + "2")         //12      
+// console.log("1" + 2 + 2);     //122
+// console.log(1 + 2 + "2");      //32
+
+
+console.log(+true);         //1
+
+console.log(+"");           //0
+
+//prefix and postfix operation
+let gameCounter = 100;
+++gameCounter;
+gameCounter++;
+
+
+ 
+
+
